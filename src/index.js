abdd239c9d03bd0ba62ce34a041c9ec20d34165e
Alejandra Soto <asoto89@gmail.com>
2022-01-22 22:31:23 +0100

    use github pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-    <Router basename="/noter">
+    <Router>
         <React.StrictMode>
             <App />
         </React.StrictMode>
